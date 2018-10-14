63440feb1518aaf312f3ba4f311fdc14610ffb18
earnesthooks <earnest.hooks@gmail.com>
2018-10-14 09:02:32 -0700

    1st Commit of Code

---
+++ A2ZPOS/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace A2ZPOS
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/lib/bootstrap/js/bootstrap.min",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/lib/bootstrap/css/bootstrap.min.css",
+                      "~/Content/style.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ A2ZPOS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A2ZPOS.MvcApplication" Language="C#" %>
