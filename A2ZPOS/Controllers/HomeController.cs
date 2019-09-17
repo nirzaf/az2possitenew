5183976923725e6a632022c457f4c530b65d65ed
Fazrin <nirzaf@gmail.com>
2019-09-17 21:51:45 +0530

    Controller

--- A2ZPOS/Controllers/HomeController.cs
+++ A2ZPOS/Controllers/HomeController.cs
@@ -1,30 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
 
-namespace A2ZPOS.Controllers
+namespace WebAPI.Controllers
 {
-    public class HomeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankAccountController : ControllerBase
     {
-        public ActionResult Index()
+        private readonly APIDBContext _context;
+
+        public BankAccountController(APIDBContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public ActionResult About()
+        // GET: api/BankAccount
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BankAccount>>> GetBankAccounts()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return await _context.BankAccounts.ToListAsync();
         }
 
-        public ActionResult Contact()
+        // GET: api/BankAccount/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BankAccount>> GetBankAccount(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
 
-            return View();
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            return bankAccount;
+        }
+
+        // PUT: api/BankAccount/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBankAccount(int id, BankAccount bankAccount)
+        {
+            if (id != bankAccount.BankAccountID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bankAccount).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BankAccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BankAccount
+        [HttpPost]
+        public async Task<ActionResult<BankAccount>> PostBankAccount(BankAccount bankAccount)
+        {
+            _context.BankAccounts.Add(bankAccount);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBankAccount", new { id = bankAccount.BankAccountID }, bankAccount);
+        }
+
+        // DELETE: api/BankAccount/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BankAccount>> DeleteBankAccount(int id)
+        {
+            var bankAccount = await _context.BankAccounts.FindAsync(id);
+            if (bankAccount == null)
+            {
+                return NotFound();
+            }
+
+            _context.BankAccounts.Remove(bankAccount);
+            await _context.SaveChangesAsync();
+
+            return bankAccount;
+        }
+
+        private bool BankAccountExists(int id)
+        {
+            return _context.BankAccounts.Any(e => e.BankAccountID == id);
         }
     }
 }
